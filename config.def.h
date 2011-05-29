48ef223d8f5dabe5ec1258948e0e2779ced7cd7c
mjheagle8 <mjheagle8@gmail.com>
2011-05-29 19:49:34 -0400

    moved patches to numbering format

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
+//static Bool useicons                = True;     /* false means use ascii symbols */
 
 /* tagging */
 static const char *tags[] = { "main", "web", "media", "chat", "other"};

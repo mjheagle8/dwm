88a654dbe0b95d4f5bcd8218350c50afda9130e8
mjheagle <mjheagle8@gmail.com>
2012-01-30 13:46:20 -0500

    applied cycle patch

--- dwm.c
+++ dwm.c
@@ -262,6 +262,9 @@ static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void zoom(const Arg *arg);
 static void bstack(Monitor *m);
+static void cycle(const Arg *arg);
+static int shifttag(int dist);
+static void tagcycle(const Arg *arg);
 
 /* variables */
 static const char broken[] = "broken";
@@ -2209,6 +2212,42 @@ bstack(Monitor *m) {
 			resize(c, m->wx + tx, m->wy + mh, w - (2*c->bw), m->wh - mh - (2*c->bw), False);
 			tx += WIDTH(c);
 		}
+}
+
+int
+shifttag(int dist) {
+   int i, curtags;
+   int seltag = 0;
+   int numtags = LENGTH(tags);
+
+   curtags = selmon->tagset[selmon->seltags];
+   for(i = 0; i < LENGTH(tags); i++) {
+       if((curtags & (1 << i)) != 0) {
+           seltag = i;
+           break;
+       }
+   }
+
+   seltag += dist;
+   if(seltag < 0)
+       seltag = numtags - (-seltag) % numtags;
+   else
+       seltag %= numtags;
+
+   return 1 << seltag;
+}
+
+void
+cycle(const Arg *arg) {
+   const Arg a = { .i = shifttag(arg->i) };
+   view(&a);
+}
+
+void
+tagcycle(const Arg *arg) {
+   const Arg a = { .i = shifttag(arg->i) };
+   tag(&a);
+   view(&a);
 }
 
 int
